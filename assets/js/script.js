fcf5b3073d8d47e8f35a3a320489286addc0e816
freelancersazal <freelancersazal@gmail.com>
2024-07-26 12:39:51 +0600

    three new pages added

--- assets/js/script.js
+++ assets/js/script.js
@@ -170,4 +170,31 @@ $(function () {
         });
     });
 
+
+    // CALENDAR JS 
+    // $(function () {
+    //     $('#calendar_js').calendar({
+    //         months: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+    //         days: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+    //     });
+    // });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
